74c5b51b975db318c5c11392a1f72afdf382a031
laxmi <laxmiyadav21@navgurukul.org>
2023-10-05 20:11:27 +0530

    UseNavigate hook in react router

--- src/About.js
+++ src/About.js
@@ -1,10 +1,19 @@
 import React from "react";
-import Header from "./Component/Header"
+import { useNavigate } from "react-router-dom";
 const About=()=>{
+    const navigate = useNavigate();
+     const goToContact =()=>{
+        navigate("/Contact")
+     }
+     const goBack=()=>{
+        navigate(-1)
+     }
     return(
     <>
         <section>
             <h1>About page</h1>
+            <button onClick={()=>goToContact()}> go to contact page</button>
+            <button onClick={()=>goBack()}>go back</button>
         </section>
     </>
     )
--- src/Contact.js
+++ src/Contact.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Contact=()=>{
+    const navigate=useNavigate();
+    const gotohome=()=>{
+        navigate("/")
+    }
     return(
     <>
         
         <section>
             <h1>Contact page</h1>
+            <button onClick={gotohome}>go to home page</button>
         </section>
     </>
     )
